6d3c5d1f96ce4a4ff121ddd2aff8e5e2a3862ed7
Mathias Sundman <mathias.sundman@it-total.se>
2024-02-25 20:13:45 +0100

    Cleaned up unused modules

--- GetKeyVaultTokenCommand.cs
+++ GetKeyVaultTokenCommand.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Management.Automation;
-using System.Management.Automation.Runspaces;
-using System.Security;
 using Azure.Identity;
 using Microsoft.Identity.Client;
 using Smartersoft.Identity.Client.Assertion;
@@ -20,31 +16,22 @@ namespace ITTotal.PowerShell.Azure.Commands
         [Parameter(Mandatory = true)] public string CertificateName { get; set; }
         [Parameter(Mandatory = false)] public string Scope { get; set; } = "https://graph.microsoft.com/.default";
 
-        // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
-        protected override void ProcessRecord()
-        {
-            // Synchronously calling the asynchronous method and waiting for its completion
-            AuthenticationResult authResult = GetTokenAsync().GetAwaiter().GetResult();
-            WriteObject(authResult);
-        }
+    protected override void ProcessRecord()
+    {
+        var tokenCredential = new DefaultAzureCredential();
 
-        private async Task<AuthenticationResult> GetTokenAsync()
-        {
-            // Create a token credential that suits your needs, used to access the KeyVault
-            var tokenCredential = new DefaultAzureCredential();
+        // Use the ConfidentialClientApplicationBuilder as usual
+        // but call `.WithKeyVaultCertificate(...)` instead of `.WithCertificate(...)`
+        var app = ConfidentialClientApplicationBuilder
+            .Create(ClientId)
+            .WithAuthority(AzureCloudInstance.AzurePublic, TenantId)
+            .WithKeyVaultCertificate(new Uri(KeyVaultUri), CertificateName, tokenCredential)
+            .Build();
 
-            // Use the ConfidentialClientApplicationBuilder as usual
-            // but call `.WithKeyVaultCertificate(...)` instead of `.WithCertificate(...)`
-            var app = ConfidentialClientApplicationBuilder
-                .Create(ClientId)
-                .WithAuthority(AzureCloudInstance.AzurePublic, TenantId)
-                .WithKeyVaultCertificate(new Uri(KeyVaultUri), CertificateName, tokenCredential)
-                .Build();
+        var authResult = app.AcquireTokenForClient(new[] { Scope })
+            .ExecuteAsync().GetAwaiter().GetResult();
 
-            var authResult = await app.AcquireTokenForClient(new[] { Scope })
-                .ExecuteAsync();
-
-            return authResult;
-        }
+        WriteObject(authResult);
+    }
     }
 }
